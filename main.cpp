0ea65df496201cec12cb8627c482465ddc596b27
andrea <andrea.assioma@stud.unifi.it>
2021-03-31 15:49:39 +0200

    Merge branch: imgage_saver. Also implemented save.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     if (b.load(imgName)) {
         std::cout << "show image: " << imgName << std::endl;
         //..change image...
-
+        b.save("new-test.jpg");
     } else
         std::cerr << "image:" << imgName << " not readable." << std::endl;
     return 0;
